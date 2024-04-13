48e39d6c278acdb653743a6341df1c9667714a53
박정규 <morimorin@plani.co.kr>
2024-04-14 05:28:09 +0900

    🔨 정기 결제 카드 관리 퍼블리싱 수정

--- resources/views/cards/card_list.blade.php
+++ resources/views/cards/card_list.blade.php
@@ -13,30 +13,23 @@
                         <div class="card">
                             <img src="{{asset("images/ex_card.png")}}" alt="">
                         </div>
-                        <div class="ellipsis_vertical">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512">
-                                <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-                                <path
-                                    d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/>
-                            </svg>
-                        </div>
                         <p class="card_name">{{$card->name}} ({{$card->number}})</p>
                     </div>
                 @endforeach
                 @if($cards->count() < 3)
                     <div class="card_box registration">
                         <div class="content">카드 등록하기</div>
-                        <p>* 정기 결제 카드는 <strong>최대 3개까지 등록</strong>가능합니다.</p>
+                        <p>* 정기 결제 카드는 <strong>최대 3개까지 등록</strong> 가능합니다.</p>
                     </div>
                 @endif
             </div>
             <div class="txt_wrap">
                 <strong>[유의 사항]</strong><br/>
-                카드 소유자 명의의 휴대폰 인증을 진행 한 후 카드 정보를 등록합니다.<br/>
-                - 개인 카드 : 등록할 카드의 소유주의 생년월일과 휴대폰 본인 인증 생년월일 일치해야 합니다.<br/>
-                - 법인 카드 : 카드 등록 시 사업자 번호 기입 후 결제 담당자의 휴대폰 번호로 인증을 받습니다.<br/><br/>
+                카드 소유자 명의의 휴대폰 인증을 진행 한 후 카드 정보를 등록합니다.<br />
+                - 개인 카드 : 등록할 카드의 소유주의 생년월일과 휴대폰 본인 인증 생년월일 일치해야 합니다.<br />
+                - 법인 카드 : 카드 등록 시 사업자 번호 기입 후 결제 담당자의 휴대폰 번호로 인증을 받습니다.<br /><br />
 
-                * 설정한 결제일대로 등록 익월부터 결제 진행됩니다.<br/>
+                * 설정한 결제일대로 등록 익월부터 결제 진행됩니다.<br />
                 * 정기 결제 카드는 <strong>최대 3개까지 등록</strong>가능합니다.
             </div>
         </div>
