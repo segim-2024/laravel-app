d4eefcb165051bf1e3d1d8269e8346028c006198
박정규 <morimorin@plani.co.kr>
2024-06-04 19:55:52 +0900

    🔨 SSO 에러 수정

--- app/Http/Controllers/SSOController.php
+++ app/Http/Controllers/SSOController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use App\Models\Member;
 use App\Services\Interfaces\MemberCashServiceInterface;
 use App\Services\Interfaces\MemberServiceInterface;
+use Exception;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Log;
+use RuntimeException;
 
 class SSOController extends Controller
 {
@@ -20,28 +22,30 @@ class SSOController extends Controller
 
     public function handleSSO(Request $request): RedirectResponse
     {
-        // SSO 인증 로직 구현
-        // 예시로, SSO 토큰을 검증하고 사용자 정보를 조회하는 과정을 가정합니다.
-
-        // SSO 토큰 검증 및 사용자 정보 조회
-        $member = $this->authenticateWithSSO($request);
-        if (! $member) {
+        // SSO 토큰을 검증하고 사용자 정보를 조회하는 과정을 가정합니다.
+        try {
+            $member = $this->authenticateWithSSO($request);
+            if (! $member) {
+                throw new RuntimeException("유효하지 않은 사용자 정보입니다.");
+            }
+        } catch (Exception $e) {
             // 인증 실패 시, 로그인 페이지나 에러 페이지로 리디렉션
-            return redirect()->route('login')->withErrors('SSO 인증 실패');
+            Log::warning($e);
+            return redirect()->route('home');
         }
 
+        // E-Cash 데이터가 없으면 생성
         if (! $member->cash) {
             $this->cashService->create($member);
         }
 
+        // Toss 고객 키가 없으면 생성
         if (! $member->toss_customer_key) {
             $this->memberService->updateTossCustomerKey($member);
         }
 
-        // Laravel 내부 인증 시스템을 사용하여 사용자 세션 생성
+        // Laravel 내부 인증 시스템 사용자 세션 생성 후 서비스 페이지 진입
         Auth::login($member);
-
-        // 인증 성공 시, /main 라우트로 리디렉션
         return redirect()->route('cards.index');
     }
 
@@ -51,22 +55,25 @@ class SSOController extends Controller
             return Member::where('mb_id', '=', 'test01')->first();
         }
 
-        $key = Config::get('services.sso.key');
-
         // 쿼리스트링에서 암호화된 데이터 받기
-        $secureData = $request->input('data');
-        [$dataEncoded, $signature] = explode('.', $secureData);
+        try {
+            $secureData = $request->input('data');
+            [$dataEncoded, $signature] = explode('.', $secureData);
 
-        // 서명 검증
-        if (hash_equals(hash_hmac('sha256', $dataEncoded, $key), $signature)) {
-            $data = json_decode(base64_decode($dataEncoded), true, 512, JSON_THROW_ON_ERROR);
-            Log::info(json_encode($data));
-            if (! $data['mb_id']) {
-                return null;
+            // 서명 검증
+            if (hash_equals(hash_hmac('sha256', $dataEncoded, Config::get('services.sso.key')), $signature)) {
+                $data = json_decode(base64_decode($dataEncoded), true, 512, JSON_THROW_ON_ERROR);
+                Log::info(json_encode($data, JSON_THROW_ON_ERROR));
+                if (! $data['mb_id']) {
+                    return null;
+                }
+
+                // 데이터 사용, 예: 사용자 인증
+                return Member::where('mb_id', '=', $data['mb_id'])->first();
             }
-
-            // 데이터 사용, 예: 사용자 인증
-            return Member::where('mb_id', '=', $data['mb_id'])->first();
+        } catch (Exception $exception) {
+            Log::warning($exception);
+            throw new RuntimeException("유효하지 않은 토큰 정보입니다.");
         }
 
         return null;
