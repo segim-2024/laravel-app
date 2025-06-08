36a2050aedaa2afd6e26086080bb7cfa4bed2d1d
Jk <74996107+freedown0305@users.noreply.github.com>
2025-06-08 21:59:24 +0900

    ✨ 이캐쉬 사용 내역 엑셀 엔드포인트

--- app/Enums/GetECashHistorySearchTypeEnum.php
+++ app/Enums/GetECashHistorySearchTypeEnum.php
@@ -5,6 +5,6 @@ namespace App\Enums;
 enum GetECashHistorySearchTypeEnum: string
 {
     case ALL = 'all';
-    case MEMBER_ID = 'member_id';
-    case ACADEMY_NAME = 'academy_name';
+    case MEMBER_ID = 'mb_id';
+    case ACADEMY_NAME = 'mb_name';
 }
